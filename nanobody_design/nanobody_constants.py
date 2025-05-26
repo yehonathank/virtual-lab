2e235d784fa99380807686ecc6b63e6e16428c1b
Kyle Swanson <swansonk.14@gmail.com>
2025-05-26 12:50:08 -0700

    Adding coding eval for single vs multi agent

--- nanobody_design/nanobody_constants.py
+++ nanobody_design/nanobody_constants.py
@@ -25,11 +25,11 @@ workflow_phases = [
     "rosetta",
     "workflow_design",
 ]
-ablation_phases = ["ablation"]
+ablations_phases = ["ablations"]
 human_eval_phases = ["human_eval"]
 finetuning_phases = ["finetuning"]
 review_phases = ["unpaired_cysteine"]
-phases = workflow_phases + ablation_phases + human_eval_phases + finetuning_phases + review_phases
+phases = workflow_phases + ablations_phases + human_eval_phases + finetuning_phases + review_phases
 discussions_phase_to_dir = {phase: discussions_dir / phase for phase in phases}
 
 # Prompts
